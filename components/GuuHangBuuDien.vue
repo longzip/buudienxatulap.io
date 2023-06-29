0498062dce8ca5112312f3b66a256dc96c1f8861
Lo Van Long <info@lovanlong.ga>
2023-06-29 12:00:15 +0700

    generate

--- components/GuuHangBuuDien.vue
+++ components/GuuHangBuuDien.vue
@@ -2,7 +2,7 @@
   <section class="relative py-20">
     <div class="container mx-auto px-4">
       <div class="items-center flex flex-wrap">
-        <div class="w-full md:w-6/12 ml-auto mr-auto px-4">
+        <div v-if="!ketQua" class="w-full md:w-6/12 ml-auto mr-auto px-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-noi-dung"
@@ -28,6 +28,7 @@
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             placeholder="Ví dụ: 0978333xxx"
+            @keyup="getDiaChiNguoiNhan"
           />
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -55,6 +56,13 @@
             type="text"
             placeholder="Ví dụ: Bưu điện xã Tự Lâp, phú mỹ, tự lập, mê linh, hà nội"
           />
+          <input
+            id="grid-dia-chi-nguoi-nhan-2"
+            v-model="donHang.diaChiNguoiNhan2"
+            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            type="text"
+            placeholder="Ví dụ: Bưu điện xã Tự Lâp, phú mỹ, tự lập, mê linh, hà nội"
+          />
           <hr class="mb-10" />
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
@@ -68,7 +76,7 @@
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             placeholder="0978333963"
-            @keydown.enter="getDiaChi"
+            @keyup="getDiaChi"
           />
           <input
             id="grid-ho-ten-nguoi-gui"
@@ -108,6 +116,45 @@
             </a>
           </div>
         </div>
+        <div v-else class="w-full md:w-6/12 ml-auto mr-auto px-4">
+          <div class="text-center mt-12">
+            <h3
+              class="text-4xl font-semibold leading-normal text-gray-800 mb-2"
+            >
+              Đơn hàng đã được sao chép vào bộ nhớ.
+            </h3>
+            <p class="mb-4 text-lg leading-relaxed text-gray-800">
+              Bấm nút Chat ở dưới và dán nội dung này (đã được copy) vào tin
+              nhắn để gửi đi (chỉ cần mở ứng dụng và gửi tin nhắn đến chị Hồ Thị
+              Thắm ở dưới).
+            </p>
+            <p class="mb-4 text-lg leading-relaxed text-gray-800">
+              {{
+                `Nội dung: ${donHang.noiDung} \r\n\n
+                  Người gửi: ${[
+                    donHang.soDienThoaiNguoiGui,
+                    donHang.hoTenNguoiGui,
+                    donHang.diaChiNguoiGui,
+                  ].join('\t')} \r\n
+                  \n
+                  Người nhận: ${[
+                    donHang.soDienThoaiNguoiNhan,
+                    donHang.hoTenNguoiNhan,
+                    donHang.diaChiNguoiNhan + ', ' + donHang.diaChiNguoiNhan2,
+                  ].join('\t')}
+              `
+              }}
+              <a
+                class="font-normal text-white shadow-md"
+                target="_blank"
+                href="https://zalo.me/0978333963"
+                ><span class="text-2xl bg-yellow-500"
+                  >Bấm vào đây để gửi qua Zalo</span
+                >
+              </a>
+            </p>
+          </div>
+        </div>
         <div class="w-full md:w-6/12 ml-auto mr-auto px-4"></div>
       </div>
     </div>
@@ -125,8 +172,9 @@ export default {
         soDienThoaiNguoiNhan: '',
         hoTenNguoiNhan: '',
         diaChiNguoiNhan: '',
+        diaChiNguoiNhan2: '',
       },
-      ketQua: null,
+      ketQua: 0,
       loi: '',
       key: '',
     }
@@ -180,14 +228,37 @@ export default {
         } catch (error) {}
       }
     },
+    async getDiaChiNguoiNhan() {
+      if (this.donHang.soDienThoaiNguoiNhan.length === 10) {
+        try {
+          const [dc, p] = await this.fetchAPIByName(
+            this.donHang.soDienThoaiNguoiNhan
+          )
+          if (dc) this.donHang.diaChiNguoiNhan = dc
+          if (p) this.donHang.hoTenNguoiNhan = p
+        } catch (error) {}
+      }
+    },
     async guiDonHang() {
-      if (
-        this.donHang.soDienThoaiNguoiNhan &&
-        this.donHang.soDienThoaiNguoiGui
-      ) {
+      const donHang = this.donHang
+      if (this.donHang.soDienThoaiNguoiGui) {
+        navigator.clipboard.writeText(
+          `Nội dung: ${donHang.noiDung} \r\n` +
+            `Người gửi: ${[
+              donHang.hoTenNguoiGui,
+
+              donHang.diaChiNguoiGui,
+              donHang.soDienThoaiNguoiGui,
+            ].join('\t')} \r\n` +
+            `Người nhận: ${[
+              donHang.hoTenNguoiNhan,
+              donHang.diaChiNguoiNhan,
+              donHang.soDienThoaiNguoiNhan + ', ' + donHang.diaChiNguoiNhan2,
+            ].join('\t')}
+              `
+        )
         try {
           const data = await this.postAPIByName()
-          console.log(data)
           this.ketQua = data
         } catch (error) {
           this.loi = error

840d3474282f0bb07a3475d0f92d25762476c32d
Lo Van Long <info@lovanlong.ga>
2023-11-18 09:06:03 +0700

    generate

--- components/TinhMucDongVaLuongHuu.vue
+++ components/TinhMucDongVaLuongHuu.vue
@@ -84,7 +84,7 @@
             </div>
             <a
               target="_blank"
-              href="https://m.me/103440001315066"
+              href="https://www.hotham.vn/dongbhxh"
               class="text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white dark:focus:ring-primary-900"
               >Bắt đầu</a
             >
